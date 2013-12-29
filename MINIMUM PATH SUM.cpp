bb824398435baa8cb47738131f876ecc8b3ec9d0
Jacky Chen <jingchen@cmu.edu>
2013-12-28 16:47:55 -0800

    finish experiment

--- MINIMUM PATH SUM.cpp
+++ MINIMUM PATH SUM.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <climits>
 using namespace std;
 
 int main (int argc, char* argv[]) 
@@ -21,12 +22,22 @@ int main (int argc, char* argv[])
 			istringstream iss(lineBuffer);
 			iss >> line;
 			iss.clear(); //reset io state
-			vector<vector<int> > v(line, vector<int>(line));
+			vector<int> v(line, INT_MAX);
 			for(int i=0; i<line; i++) {
+				int num;
 				getline(file, lineBuffer);
 				iss.str(lineBuffer);
+				iss >> num;
+				if(i == 0) v[0] = num;
+				else v[0] += num;
+				for(int j=1; j<line; j++) {
+					iss.ignore(1);
+					iss >> num;
+					v[j] = min(v[j], v[j-1]) + num;
+				}
+				iss.clear(); //reset io state
 			}
-
+			cout << v[line-1] << endl;
 		}
 	}
 	
